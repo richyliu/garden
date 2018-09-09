1bd89b0efa0754d2f1a854f0bee7645da66db259
richyliu <richy.liu.2002@gmail.com>
2018-09-08 19:11:22 -0700

    add firebase

--- src/js/database.js
+++ src/js/database.js
@@ -1,5 +1,27 @@
 // contains data that is synced with firebase
 
-firebase.config({
+import firebase from 'firebase/app';
+import 'firebase/firestore'
 
-})
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA1G05BhsyLuC5HqUYvfOVbDjKzxiC-Pts",
+  authDomain: "garden-c398b.firebaseapp.com",
+  projectId: "garden-c398b"
+});
+
+const db = firebase.firestore();
+
+db.settings({
+  timestampsInSnapshots: true
+});
+
+db.collection('test').add({
+  first: 'foo',
+  last: 'bar',
+  age: 21
+}).then(docRef => {
+  console.log('docRef', docRef);
+}).catch(error => {
+  console.log('error adding doc', error);
+});
